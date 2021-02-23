9bc862b141ac4a2e5a1b29bc758878278bd501c3
Len.Xu <xuyapeng@inspur.com>
2021-02-23 08:50:06 +0800

    update chain of resposibility mode

--- 设计模式/chain_of_resposibility.py
+++ 设计模式/chain_of_resposibility.py
@@ -3,7 +3,7 @@
 '''
 Author: Len-xu
 Date: 2021-02-23 08:29:56
-LastEditTime: 2021-02-23 08:47:28
+LastEditTime: 2021-02-23 08:49:41
 LastEditors: Do not edit
 Description: 责任链模式
 '''
@@ -23,22 +23,22 @@ class GeneralManager(Handler):
 
 class DepartmentManager(Handler):
     def __init__(self) -> None:
-        self.root_handler = GeneralManager()
+        self.next = GeneralManager()  # 对象以组合的方式形成一条处理链
     def handle_leave(self, day):
         if day <=5:
             print("部门经理准假%d天" % day)
         else:
-            self.root_handler.handle_leave(day)
+            self.next.handle_leave(day)
 
 class ProjectDirector(Handler):
     def __init__(self) -> None:
-        self.root_hander = DepartmentManager()
+        self.next = DepartmentManager()  # 对象以组合的方式形成一条处理链
 
     def handle_leave(self, day):
         if day <= 3:
             print("项目经理准假%d天" % day)
         else:
-            self.root_hander.handle_leave(day)
+            self.next.handle_leave(day)
 
 p = ProjectDirector()
 
